205df42a06a66e610757fbf952a2390a7ccd28b2
Yao Hu <huyaohit@163.com>
2016-11-17 18:40:16 -0500

    testing mysql

--- Secret_Server/src/main/java/Server.java
+++ Secret_Server/src/main/java/Server.java
@@ -4,38 +4,40 @@ import io.undertow.server.handlers.*;
 import io.undertow.util.Headers;
 import java.sql.*;
 import java.util.*;
+import java.sql.*;
 
 public class Server {
 	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_NAME = "phase2";
-
-    private static final String URL = "jdbc:mysql://ec2-54-165-15-232.compute-1.amazonaws.com:3306/" + DB_NAME +"?useUnicode=true&characterEncoding=utf-8&autoReconnectForPools=true";
-
-    private static final String DB_USER = "root";
-    private static final String DB_PWD = "12345678";
-
-    @SuppressWarnings("finally")
-	private static String query(Connection conn,String userid,String hashtag,int mod) {
+    private static final String URL = "jdbc:mysql://localhost:3306/secrets?user=root&password=123456&useUnicode=true&characterEncoding=utf-8&autoReconnectForPools=true";
+    
+    private static Connection getConnection() {
+    	Connection conn = null;
+    	
+    	try {
+    		Class.forName(JDBC_DRIVER);
+    		conn = DriverManager.getConnection(URL);
+    	} catch (SQLException ex) {
+    		ex.printStackTrace();
+    	} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+    	
+    	return conn;
+    }
+    
+	private static String doCreate(String name, String secret) {
         Statement stmt = null;
-        String result="";
+        Connection conn = getConnection();
 
         try {
             stmt = conn.createStatement();
-            String sql = "SET NAMES utf8mb4";
+            String sql = "INSERT INTO TABLE secrets_table(user_name, secrets_content) VALUES("
+            		+ name + ", " + secret + ")";
             stmt.executeUpdate(sql);
-
-            String tableName="twitter"+mod;
-            
-            sql = "SELECT sentimental_density,post_time,twitter_id,censored_text FROM "+tableName+
-            	" WHERE user_id='"+userid+
-            	"' AND hash_tag='"+hashtag+"'"+
-            	" ORDER BY sentimental_density DESC,post_time ASC,twitter_id ASC";
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-            	result+=String.format("%.3f",rs.getDouble(1))+":"+rs.getString(2)+":"+rs.getString(3)+":"+rs.getString(4)+"\n";
-            }
+            return "create secret succeeds";
         } catch (SQLException e) {
             e.printStackTrace();
+            return "create secret fails";
         } finally {
             if (stmt != null) {
                 try {
@@ -44,9 +46,6 @@ public class Server {
                     e.printStackTrace();
                 }
             }
-            if(result.equals(""))
-            	result="\n\n";
-            return result;
         }
     }
 
@@ -61,14 +60,24 @@ public class Server {
 		            }
 					
                     Map<String, Deque<String>> parameters = exchange.getQueryParameters();
-
                     Deque<String> q1 = parameters.get("name");
                     String name = (String)q1.poll();
-
                     Deque<String> q2 = parameters.get("secret");
                     String secret = (String)q2.poll();
+                    
+                    String response = doCreate(name, secret);
 
-					exchange.getResponseSender().send(name + "\n" + secret);
+					exchange.getResponseSender().send(response);
+                }
+    		})
+    		.addPrefixPath("view",new HttpHandler(){
+    			public void handleRequest(final HttpServerExchange exchange) throws Exception {
+					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+
+					if (exchange.isInIoThread()) {
+		              exchange.dispatch(this);
+		              return;
+		            }
                 }
     		})
     		.addPrefixPath("update",new HttpHandler(){
@@ -91,6 +100,7 @@ public class Server {
 		            }
                 }
     		});
+
         Undertow server = Undertow.builder()
                 .addHttpListener(80, "ec2-52-207-220-237.compute-1.amazonaws.com")
                 .setHandler(path)
